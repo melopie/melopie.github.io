7e3a7949876246ea1b1ceb5654f15bc06b0e5155
Nisha Bhatia <nishabhatia1998@gmail.com>
2015-07-17 13:50:56 -0700

    Update game.js

--- game.js
+++ game.js
@@ -140,23 +140,7 @@ sendBtn.addEventListener("click", function (evt) {
     //console.log("sent");
     
     
-    console.log("Inside showInput function");
-    numPlayers = document.getElementById("numPlayersID").value;
-    console.log("Number of players: " + numPlayers);
-    var playerNames = [];
-    playerNames = document.getElementsByClassName("fname1");
-    //console.log(playerNames);
-    // console.log(playerNames[0]);
-    // console.log(playerNames[0].value);
-    for (var i = 0; i < numPlayers; i++) {
-        roundNum = i;
-        playerName = playerNames[i].value; //get player name
-        dict[playerName] = roundNum; //add player and turn number to dictionary
-
-        console.log("Player Name: " + playerName);
-        console.log("Turn number: " + i);
-    }
-    console.log(dict);
+    showInput();
     
 });
 
