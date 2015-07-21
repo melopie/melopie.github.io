30b407bbe37f2fab63aadea635bce98ad4fbd8af
same lame <samantha.lam@sfuhs.org>
2015-07-21 11:46:34 -0700

    Update game.js

--- game.js
+++ game.js
@@ -1,40 +1,39 @@
 var canvas = document.getElementById("drawingPad");
-var ctx = canvas.getContext("2d");
+var context = canvas.getContext("2d");
 var isMouseDown = false;
 var mouseX = 0;
 var mouseY = 0;
-var dict = [];
 
-ctx.strokeStyle = "#000000"; // drawing black lines.
+context.strokeStyle = "#000000"; // drawing black lines.
 
 // make sure the canvas' background is actually white for saving.
-ctx.fillStyle = "#FCF0AD";
-ctx.fillRect(0, 0, canvas.width, canvas.height);
+context.fillStyle = "#ffffff";
+context.fillRect(0,0,canvas.width,canvas.height);
 
 // when the user presses their mouse down on the canvas.
-canvas.addEventListener("mousedown", function (evt) {
+canvas.addEventListener("mousedown",function (evt) {
     isMouseDown = true;
 
     mouseX = evt.offsetX;
     mouseY = evt.offsetY;
 
-    ctx.beginPath();
-    ctx.moveTo(mouseX, mouseY);
+    context.beginPath();
+    context.moveTo(mouseX, mouseY);
 });
 
 // when the user lifts their mouse up anywhere on the screen.
-window.addEventListener("mouseup", function (evt) {
+window.addEventListener("mouseup",function (evt) {
     isMouseDown = false;
 });
 
 // as the user moves the mouse around.
-canvas.addEventListener("mousemove", function (evt) {
+canvas.addEventListener("mousemove",function (evt) {
     if (isMouseDown) {
         mouseX = evt.offsetX;
         mouseY = evt.offsetY;
 
-        ctx.lineTo(mouseX, mouseY);
-        ctx.stroke();
+        context.lineTo(mouseX, mouseY);
+        context.stroke();
     }
 });
 
@@ -43,70 +42,55 @@ var palette = document.getElementById("palette");
 var swatches = palette.children;
 var currentSwatch; // we'll keep track of what swatch is active in this.
 
-	for (var i = 0; i < swatches.length; i++) {
-	    var swatch = swatches[i];
-    		if (i === 0) {
-        		currentSwatch = swatch;
-    	}
+for (var i = 0; i < swatches.length; i++) {
+    var swatch = swatches[i];
+    if (i == 0) {
+        currentSwatch = swatch;
+    }
 
     // when we click on a swatch...
-    	swatch.addEventListener("click", function (evt) {
+    swatch.addEventListener("click",function (evt) {
 
-        	this.className = "active"; // give the swatch a class of "active", which will trigger the CSS border.
-	 	currentSwatch.className = ""; // remove the "active" class from the previously selected swatch
-        	currentSwatch = this; // set this to the current swatch so next time we'll take "active" off of this.
+        this.className = "active"; // give the swatch a class of "active", which will trigger the CSS border.
+        currentSwatch.className = ""; // remove the "active" class from the previously selected swatch
+        currentSwatch = this; // set this to the current swatch so next time we'll take "active" off of this.
 
-        	ctx.strokeStyle = this.style.backgroundColor; // set the background color for the canvas.
-    	});
-	}
+        context.strokeStyle = this.style.backgroundColor; // set the background color for the canvas.
+    });
+}
 
 // when the clear button is clicked
 var clearBtn = document.getElementById("clear");
-clearBtn.addEventListener("click", function (evt) {
-    console.log("clear button clicked");
+clearBtn.addEventListener("click",function(evt) {
     canvas.width = canvas.width; // this is all it takes to clear!
 
     // make sure the canvas' background is actually white for saving.
-    ctx.fillStyle = "#FCF0AD";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    context.fillStyle = "#ffffff";
+    context.fillRect(0,0,canvas.width,canvas.height);
 });
 
 // when the save button is clicked
 var saveBtn = document.getElementById("save");
-saveBtn.addEventListener("click", function (evt) {
+saveBtn.addEventListener("click",function (evt) {
     // we'll save using the new HTML5 download attribute to save the image. 
     // we'll give the image a name of draw-[timestamp].jpg
 
     var now = new Date().getTime(); // get today's date in milliseconds.
-    var dataUri = canvas.toDataURL("image/jpeg"); // get the canvas data as a JPG.
+    var dataUri = canvas.toDataURL("image/jpeg");  // get the canvas data as a JPG.
 
     // change the a href and download attributes so it'll save.
-    this.setAttribute("download", "drawing-" + now + ".jpg");
-    this.setAttribute("href", dataUri);
+    this.setAttribute("download","drawing-" + now + ".jpg");
+    this.setAttribute("href",dataUri);
 
     // in older browsers you may need to substitute those last two lines of code with this:
     // window.open(dataUri,"_blank");
 });
 
-//send function
+//when the send button is clicked
 var sendBtn = document.getElementById("send");
-sendBtn.addEventListener("click", function (evt) {
-    console.log("send button clicked");
-   /* var picture1 = canvas.toDataURL("image/jpeg");
-    dict.push({
-    	key: canvas,
-  	value: picture1
-    });
-    return picture1;
-    console.log("end of send");   
-	//console.log(dict);*/
+sendBtn.addEvenListener("click",function (evt) {
+	console.log("send clicked");
 });
-
-/*var readyBtn = document.getElementById("ready");
-readyBtn.addEventListener("click", function (evt) {
-   console.log("ready button clicked");
-});*/
-
 // SECOND DRAWING PAD
 
 
