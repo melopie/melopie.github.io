3627700d52f8b6a29bac1d104f43f4e7b723cc9c
same lame <samantha.lam@sfuhs.org>
2015-07-21 11:52:59 -0700

    Update game.js

--- game.js
+++ game.js
@@ -93,43 +93,42 @@ sendBtn.addEvenListener("click",function (evt) {
 });
 // SECOND DRAWING PAD
 
-
-var canvas2 = document.getElementById("drawingPad2");
-var ctx2 = canvas2.getContext("2d");
+var canvas2 = document.getElementById("drawingPad");
+var context2 = canvas2.getContext("2d");
 var isMouseDown2 = false;
 var mouseX2 = 0;
 var mouseY2 = 0;
 
-ctx2.strokeStyle = "#000000"; // drawing black lines.
+context2.strokeStyle = "#000000"; // drawing black lines.
 
 // make sure the canvas' background is actually white for saving.
-ctx2.fillStyle = "#FCF0AD";
-ctx2.fillRect(0, 0, canvas2.width, canvas2.height);
+context2.fillStyle = "#ffffff";
+context2.fillRect(0,0,canvas.width,canvas.height);
 
 // when the user presses their mouse down on the canvas.
-canvas2.addEventListener("mousedown", function (evt) {
+canvas2.addEventListener("mousedown",function (evt) {
     isMouseDown2 = true;
 
     mouseX2 = evt.offsetX;
     mouseY2 = evt.offsetY;
 
-    ctx2.beginPath();
-    ctx2.moveTo(mouseX2, mouseY2);
+    context2.beginPath();
+    context2.moveTo(mouseX, mouseY);
 });
 
 // when the user lifts their mouse up anywhere on the screen.
-window.addEventListener("mouseup", function (evt) {
+window.addEventListener("mouseup",function (evt) {
     isMouseDown2 = false;
 });
 
 // as the user moves the mouse around.
-canvas2.addEventListener("mousemove", function (evt) {
+canvas2.addEventListener("mousemove",function (evt) {
     if (isMouseDown2) {
         mouseX2 = evt.offsetX;
         mouseY2 = evt.offsetY;
 
-        ctx2.lineTo(mouseX2, mouseY2);
-        ctx2.stroke();
+        context2.lineTo(mouseX2, mouseY2);
+        context2.stroke();
     }
 });
 
@@ -145,55 +144,46 @@ for (var j = 0; j < swatches.length; j++) {
     }
 
     // when we click on a swatch...
-    swatch2.addEventListener("click", function (evt) {
+    swatch2.addEventListener("click",function (evt) {
 
         this.className = "active"; // give the swatch a class of "active", which will trigger the CSS border.
         currentSwatch2.className = ""; // remove the "active" class from the previously selected swatch
         currentSwatch2 = this; // set this to the current swatch so next time we'll take "active" off of this.
 
-        ctx2.strokeStyle = this.style.backgroundColor; // set the background color for the canvas.
+        context2.strokeStyle = this.style.backgroundColor; // set the background color for the canvas.
     });
 }
 
 // when the clear button is clicked
 var clearBtn2 = document.getElementById("clear2");
-clearBtn2.addEventListener("click", function (evt) {
+clearBtn2.addEventListener("click",function(evt) {
     canvas2.width = canvas2.width; // this is all it takes to clear!
 
     // make sure the canvas' background is actually white for saving.
-    ctx2.fillStyle = "#FCF0AD";
-    ctx2.fillRect(0, 0, canvas2.width, canvas2.height);
+    context2.fillStyle = "#ffffff";
+    context2.fillRect(0,0,canvas2.width,canvas2.height);
 });
 
 // when the save button is clicked
 var saveBtn2 = document.getElementById("save2");
-saveBtn2.addEventListener("click", function (evt) {
+saveBtn2.addEventListener("click",function (evt) {
     // we'll save using the new HTML5 download attribute to save the image. 
     // we'll give the image a name of draw-[timestamp].jpg
 
     var now2 = new Date().getTime(); // get today's date in milliseconds.
-    var dataUri2 = canvas2.toDataURL("image/jpeg"); // get the canvas data as a JPG.
+    var dataUri2 = canvas2.toDataURL("image/jpeg");  // get the canvas data as a JPG.
 
     // change the a href and download attributes so it'll save.
-    this.setAttribute("download", "drawing-" + now + ".jpg");
-    this.setAttribute("href", dataUri2);
+    this.setAttribute("download","drawing-" + now + ".jpg");
+    this.setAttribute("href",dataUri2);
 
     // in older browsers you may need to substitute those last two lines of code with this:
     // window.open(dataUri,"_blank");
 });
 
-//send function
-var sendBtn2 = function () {
-sendBtn2.addEventListener("click", function (evt) {
-    /*var picture = canvas2.toDataURL("image/jpeg");
-    	this.setAttribute("href", dataUri2);
-    	dict.push({
-    		key: canvas2,
-    		value: picture2
-    	});
-    	return picture2;
-	console.log("end of send");   
-	//console.log(dict);*/
+var sendBtn = document.getElementById("send2");
+sendBtn2.addeEventListener("click", function (evt) {
+	console.log("send2 clicked");
 });
 
 
